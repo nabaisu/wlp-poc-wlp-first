8b90f7f6542447db584eb2a63d1a1e432963e3cd
Joao Nabais <35026036+nabaisu@users.noreply.github.com>
2022-07-20 17:32:51 +0100

    Update index.js

--- src/wlp/index.js
+++ src/wlp/index.js
@@ -1,3 +1,3 @@
 export const a = "this is a successful test! I'm in White Label!!";
-export const b = "This area should be RED! I'm in the WLP. another test. 1";
+export const b = "This area should be RED! I'm in the WLP. another test.";
 export const c = 10;
