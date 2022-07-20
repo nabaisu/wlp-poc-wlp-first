cfc80d499657b8d513fda8c91c7c75529889ac7a
Joao Nabais <jnabais@icapitalnetwork.com>
2022-07-20 16:15:42 +0100

    pitfall to wlp

--- src/wlp/index.js
+++ src/wlp/index.js
@@ -1,3 +1,3 @@
-export const a = "this is a test, I'm in main";
+export const a = "this is a pitfall to the white label partner, did you fall ?";
 export const b = "this should be inside a gray area in main";
 export const c = 2;
