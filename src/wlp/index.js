d9862bd089ee62303d8c85c8ff386e150641e1d9
Joao Nabais <35026036+nabaisu@users.noreply.github.com>
2022-07-20 16:37:59 +0100

    Update index.js

--- src/wlp/index.js
+++ src/wlp/index.js
@@ -1,3 +1,3 @@
 export const a = "they didn't let it pass, congrats ! I should only be in main";
-export const b = "this should be inside a gray area in main";
+export const b = "this should be inside a gray area in main. t";
 export const c = 2;
