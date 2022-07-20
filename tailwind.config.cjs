082ef7a9015c104c1c2cfd767a37f2a43b3ffa32
Joao Nabais <jnabais@icapitalnetwork.com>
2022-07-20 18:00:04 +0100

    update colors as well

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("./src/wlp/colors.cjs");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      gray: {
-        100: "#f3f4f6",
-        200: "#e5e7eb",
-        300: "#d1d5db",
-        400: "#9ca3af",
-        500: "#6b7280",
-        600: "#4b5563",
-        700: "#374151",
-        800: "#1f2937",
-        900: "#111827",
-      },
-    },
+    colors: colors,
     extend: {},
   },
   plugins: [],
