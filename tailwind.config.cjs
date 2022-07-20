d27723563cc788176a6367a9bd003877679d544d
Joao Nabais <jnabais@icapitalnetwork.com>
2022-07-20 18:13:02 +0100

    fix something

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,19 +5,7 @@ const colors = require("./src/wlp/colors.cjs");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      gray: {
-        100: "#fee2e2",
-        200: "#fecaca",
-        300: "#fca5a5",
-        400: "#f87171",
-        500: "#ef4444",
-        600: "#dc2626",
-        700: "#b91c1c",
-        800: "#991b1b",
-        900: "#7f1d1d",
-      },
-    },
+    colors: colors,
     extend: {},
   },
   plugins: [],
